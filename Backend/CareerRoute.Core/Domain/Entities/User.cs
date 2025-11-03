48857a27f7fafc8d94003d52ee8d9db516bcb497
unknown <alyaaahmed0643@gmail.com>
2025-11-03 21:29:37 +0200

    feat(infrastructure):update user entity with migrations

--- Backend/CareerRoute.Core/Domain/Entities/User.cs
+++ Backend/CareerRoute.Core/Domain/Entities/User.cs
@@ -16,7 +16,16 @@ namespace CareerRoute.Core.Domain.Entities
         public DateTime RegistrationDate { get; set; } = DateTime.UtcNow;
         public DateTime? LastLoginDate { get; set; }
         public bool IsActive { get; set; } = true;
+
+
+        [MaxLength(200), Required]
+        public string CareerGoal { get; set; }
+
+        [MaxLength(200), Required]
+        public string CareerInterst { get; set; }
+
         public virtual List<RefreshToken> RefreshTokens { get; set; } = [];
+
 
 
 
