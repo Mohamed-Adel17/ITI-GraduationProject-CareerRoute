274b6859073c51758de1fc8f3961dd53f41ee8d1
Mohamed Shoaib <mohamedshoaib488@gmail.com>
2025-11-08 16:48:03 +0200

    apply changes

--- Frontend/src/app/app.ts
+++ Frontend/src/app/app.ts
@@ -11,3 +11,32 @@ import { NotificationComponent } from './shared/components/notification/notifica
 export class App {
   protected readonly title = signal('Frontend');
 }
+
+// import { Component } from '@angular/core';
+// import { RouterOutlet } from '@angular/router';
+// import { provideRouter } from '@angular/router';
+// import { bootstrapApplication } from '@angular/platform-browser';
+// import { importProvidersFrom } from '@angular/core';
+// import { LoginComponent } from './pages/login/login';
+// import { Routes } from '@angular/router';
+
+// const routes: Routes = [
+//   { path: '', redirectTo: 'login', pathMatch: 'full' },
+//   { path: 'login', component: LoginComponent },
+//   { path: '**', redirectTo: 'login' }
+// ];
+
+// @Component({
+//   selector: 'app-root',
+//   standalone: true,
+//   imports: [RouterOutlet],
+//   template: `<router-outlet></router-outlet>`
+// })
+// export class AppComponent {}
+
+// // ✅ bootstrap التطبيق مع الراوتس
+// bootstrapApplication(AppComponent, {
+//   providers: [
+//     provideRouter(routes),
+//   ],
+// });
---
+++ Frontend/src/app/pages/login/login.ts
@@ -0,0 +1,20 @@
+// import { Component } from '@angular/core';
+// import { CommonModule } from '@angular/common';
+// import { FormsModule } from '@angular/forms';
+
+// @Component({
+//   selector: 'app-login',
+//   standalone: true,
+//   imports: [CommonModule, FormsModule],
+//   templateUrl: './login.html',
+//   styleUrls: ['./login.css']
+// })
+// export class LoginComponent {
+//   email = '';
+//   password = '';
+
+//   onSubmit() {
+//     console.log('Email:', this.email);
+//     console.log('Password:', this.password);
+//   }
+// }
